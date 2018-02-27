4dd6f00aebfd26087508abffc88e296a3d70cb01
huzhengbiao <hans.hu@livestar.com>
2018-02-27 10:23:09 +0800

    增加facebook stetho查看数据库中数据

--- app/src/main/java/com/cdut/hzb/greendaodemo/App.java
+++ app/src/main/java/com/cdut/hzb/greendaodemo/App.java
@@ -4,6 +4,7 @@ import android.app.Application;
 
 import com.cdut.hzb.greendaodemo.db.DBService;
 import com.cdut.hzb.greendaodemo.db.ServiceFactory;
+import com.facebook.stetho.Stetho;
 
 /**
  * Created by hans on 2018/2/27 0027.
@@ -18,5 +19,7 @@ public class App extends Application {
         DBService dbService = ServiceFactory.getDbService();
         dbService.init(this);
 
+
+        Stetho.initializeWithDefaults(this); // 使用默认配置
     }
 }
